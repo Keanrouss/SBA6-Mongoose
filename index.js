58158d787f714131b3b68250f00524a0b760e3f6
Keanu Rouss <timajori1@gmail.com>
2025-06-20 14:23:52 -0400

    2th

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import mongoose from "mongoose"
+import User from "./model/users"
+
+const app = express()
+
+import 'dotenv/config'
+const PORT= process.env.PORT
+
+//const URL = await mongoose.connect('mongodb+srv://Ury:2021@clusterlearning.bfjwlt6.mongodb.net/Mycats?retryWrites=true&w=majority&appName=ClusterLearning')
+// const PORT = 3000
+await mongoose.connect(process.env.MONGO_URL) //added after adding the env isolation method.
+console.log("connected")
+//middleware to read JSON
+app.use (express.json());
+
+app.get('/', (req, res)=> {
+    res.send('Hello Mongoose')
+}),
+//to get all of users
+app.get ('/users',async(req,res)=>{
+    let data = await User.find({})
+res.send(data)
+})
+
+app.listen(3000, () => {
+    console.log(`Listening on port http://localhost:${3000}`);
+})
