51aee00ad1ff9503adffdcdbf63904fc887423ea
Keanu Rouss <timajori1@gmail.com>
2025-06-24 03:17:13 -0400

    9th

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import {Router} from 'express';
 import Client from '../model/user.js'
 const userRouter = Router()
 
-//to get all of users
+// create an user
 userRouter.post ('/', async (req, res, next)=>{
 try {
     const newUser = new Client(req.body)
@@ -13,11 +13,16 @@ try {
     next(error)
 }
 })
-
+//to get all of users
 userRouter.get('/', async (req,res, next)=>{
     res.json(await newUser.find())
 
 });
+//patch so users can update info
+userRouter.patch('/:id', async (req, res) => {
+    res.json(await Client.findByIdAndUpdate(req.params.id, req.body))
+});
+
 userRouter.delete('/:id', async (req, res) => {
   try {
    const Client = await Client.findByIdAndDelete(req.params.id);
